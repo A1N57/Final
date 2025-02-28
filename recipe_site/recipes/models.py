a84ef4eb883a7279d218937f3a81519572ec2ceb
alexei <lexa_nosov99@mail.ru>
2025-02-28 17:48:59 +0500

    Финальное исправление

--- recipe_site/recipes/models.py
+++ recipe_site/recipes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Recipes(models.Model):
     title = models.CharField('Название', max_length=100)
     description = models.TextField('Описание', default='')
